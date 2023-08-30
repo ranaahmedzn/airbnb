55544a5b87d565400a25b105e0465f41a4681a47
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-08-30 09:30:22 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">
+        Welcome to AirBNB!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
